c0870025fd8efea0fe10c74dde1fdce0f3e325c2
Choiyu330 <jenny4063@naver.com>
2023-05-15 15:32:12 +0900

    chore: Git 초기 세팅

---
+++ src/main/java/com/java/everyboard/EveryboardApplication.java
@@ -0,0 +1,13 @@
+package com.java.everyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/everyboard/EveryboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.everyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
