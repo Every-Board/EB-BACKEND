0fabdbd434e4ecfb7d66802eab14c120b050abb7
kangharyeom <kangharyeom@gmail.com>
2023-05-16 15:20:10 +0900

    feat: ContentPatchDto 생성

---
+++ src/main/java/com/java/everyboard/content/dto/ContentPatchDto.java
@@ -0,0 +1,21 @@
+package com.java.everyboard.content.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@Getter
+public class ContentPatchDto {
+    private Long contentId;
+    @NotBlank(message = "게시글 제목을 입력해야 합니다.")
+    private String title;
+    @NotBlank(message = "게시글의 내용을 입력해야 합니다.")
+    private String content;
+    private String imageUrl; // 컨텐츠 이미지를 담는 주소
+    @NotNull
+    private String category; // Enum으로 교체될 예정
+    private String tag;
+}
