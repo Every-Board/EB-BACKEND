ea892d30c679ab44919547d0b0f9d17ab1ef5d97
kangharyeom <kangharyeom@gmail.com>
2023-05-16 18:48:22 +0900

    feat: ContentRepository 구현

---
+++ src/main/java/com/java/everyboard/content/repository/ContentRepository.java
@@ -0,0 +1,16 @@
+package com.java.everyboard.content.repository;
+
+import com.java.everyboard.content.contentenum.Category;
+import com.java.everyboard.content.entity.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ContentRepository extends JpaRepository<Content, Long> {
+    @Query(value = "select * from content where category = :category", nativeQuery = true)
+    List<Content> findAllByCategory(Category category);
+
+    @Query(value = "select * from contents where user_id = :userId", nativeQuery = true)
+    List<Content> findAllByUserId(long userId);
+}
