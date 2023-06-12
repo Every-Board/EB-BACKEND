db041a3f11d9b0080eaeb6d6d0f8c702f356b118
kangharyeom <kangharyeom@gmail.com>
2023-06-12 13:26:12 +0900

    Refactor: ContentMapper
    - content 단건 조회 수정 (contentAllResponseDto)로 변경

--- src/main/java/com/java/everyboard/comment/repository/CommentRepository.java
+++ src/main/java/com/java/everyboard/comment/repository/CommentRepository.java
@@ -3,12 +3,13 @@ package com.java.everyboard.comment.repository;
 import com.java.everyboard.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(value = "select * from comments where content_id = :contentId", nativeQuery = true)
-    List<Comment> findAllByContentId(long contentId);
+    List<Comment> findAllByContentId(@Param("contentId") long contentId);
 
     @Query(value = "select * from comments where user_id = :userId", nativeQuery = true)
     List<Comment> findAllByUserId(long userId);
